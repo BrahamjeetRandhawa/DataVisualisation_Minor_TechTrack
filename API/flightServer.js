22bdbc2b6005c39e8bfe1a2b19eadff7752b5b32
Braham <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-10-28 11:06:37 +0100

    Update

--- API/flightServer.js
+++ API/flightServer.js
@@ -1,16 +1,26 @@
 
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const json = require("sveltejs/kit");
+import { json } from '@sveltejs/kit';
+
+// const json = require("sveltejs/kit");
 
 
 const Flight_URL = 'https://opensky-network.org/api/states/all';
 
-curl -H "Authorization: Bearer $TOKEN" https://opensky-network.org/api/states/all | jq .
+export async function GET() {
+    const response = await fetch(Flight_URL);
 
-export TOKEN=${curl - x POST "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token"} \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "grant_type=client_credentials" \
-    -d "client_id=$CLIENT_ID" \
-    -d "client_secret=$CLIENT_SECRET" | jq -r .access_token)
+    const data = await fetch(Flight_URL);
+
+    return json(flights);
+}
+
+// curl -H "Authorization: Bearer $TOKEN" https://opensky-network.org/api/states/all | jq .
+
+// export TOKEN=${curl - x POST "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token"} \
+//     -H "Content-Type: application/x-www-form-urlencoded" \
+//     -d "grant_type=client_credentials" \
+//     -d "client_id=$CLIENT_ID" \
+//     -d "client_secret=$CLIENT_SECRET" | jq -r .access_token)
