beae3e706a3f8e267b7b0adc5398b66c81424d76
BrahamjeetRandhawa <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-11-05 12:08:33 +0100

    Globe rotate working!

--- SvelteFT/src/routes/+page.svelte
+++ SvelteFT/src/routes/+page.svelte
@@ -27,8 +27,10 @@
         .scale(height / 2)
         .translate([width / 2, height / 2])
 
+        const currentZoom = projection.scale()
 
-        const path = d3.geoPath(projection);
+
+        let path = d3.geoPath(projection);
 
         const svg = d3.select("#globe");
 
@@ -43,7 +45,7 @@
         svg.append("path")
         .datum({type: "Sphere"})
         .attr("d", path)
-        .attr("fill", "blue")
+        .attr("fill", "#2B65EC")
         .attr("stroke", "black")
         .attr("stroke-width", 1)
 
@@ -61,7 +63,54 @@
             // .attr("d", d3.geoPath())
             .attr("fill", "grey")
             .attr("stroke", "black")
+
+
+
+
+        const drag = d3.drag()
+        .on("start", (/** @type {any} */event) => {
+            event.subject.rotate = projection.rotate()
+        })
+        .on("drag", (/** @type {any} */event) => {
+            const sensitivity = 0.25
+            const currentRotate = projection.rotate()
+            // const rotate = event.subject.rotate
+            const k = sensitivity
+        
+
+        projection.rotate([
+            currentRotate[0] + event.dx * k,
+            currentRotate[1] - event.dy * k
+        ]);
+
+        path = d3.geoPath(projection)
+
+        svg.selectAll("path")
+        .attr("d", path)
+
+        })
+
+        const zoom = d3.zoom()
+        .on("start",  (/** @type {any} */ event) => {
+            event.subject.zoom = projection.scale()
+        })
+        .on("zoom", (/** @type {any} */event) => {
+            const currentZoom = projection.zoom()
+
+            projection.zoom([
+                currentZoom[0] + event.zoom,
+                currentZoom[1] - event.zoom
+            ])
+        })
+
+        svg
+        .call(drag)
+        .call(zoom);
+    
+        
     })
+
+
 
     </script>
     
