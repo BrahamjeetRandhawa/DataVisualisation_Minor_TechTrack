6edf5311186095ce6e9ed1bfd939ede92db222d0
Braham <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-11-18 11:00:42 +0100

    Globe is now full screen!

--- SvelteFT/src/routes/+page.svelte
+++ SvelteFT/src/routes/+page.svelte
@@ -14,8 +14,11 @@
 
     import { onMount } from 'svelte'
     
-    const width = 1200;
-    const height = 1200;
+    // const width = 1200;
+    // const height = 1200;
+
+    let width;
+    let height;
 
 
     let allFlights = [];
@@ -28,13 +31,138 @@
     let intervalID;
 
 
+    $: if (svg && projection && width && height) {
+        svg
+        .attr("width", width)
+        .attr("height", height);
+
+        projection
+        .translate([width / 2, height / 2]);
+
+        path = d3.geoPath(projection);
+
+        svg.select(".sphere")
+        .attr("d", path);
+
+        svg.selectAll(".country")
+        .attr("d", path);
+
+        updateFlights();
+    }
+
+
+
+
+    const visiblethreshold = Math.PI / 2;
+
+    function updateFlights() {
+        if (!svg || !projection) return;
+
+        const center =[-projection.rotate()[0], -projection.rotate()[1]];
+        const iconSize = 30;
+
+        svg.selectAll("image.flight")
+        .each(function(d) {
+            const isVisible = d3.geoDistance(d, center) <= visiblethreshold;
+
+            if (isVisible) {
+
+                const [x, y] = projection(d);
+                d3.select(this)
+                .attr("x", x - (iconSize / 2))
+                .attr("y", y - (iconSize / 2))
+                .style("display", "block");
+            } else {
+                d3.select(this)
+                .style("display", "none")
+            }
+        })
+    }
+
+
+
+
+
+
+
+
+    function drawFlightsOnGlobe(flights) {
+        const coordinates = flights
+        .filter(flight => flight[5] != null && flight[6] != null)
+        .map(flight => [flight[5], flight[6]]);
+
+
+        const iconSize = 30;
+
+        svg.selectAll("image.flight")
+        .data(coordinates)
+        .join(enter => enter.append("image")
+            .attr("class", "flight")
+            .attr("href", "/flight-plane-svgrepo-com.svg")
+            .attr("width", iconSize)
+            .attr("height", iconSize),
+            // .attr("r", 2)
+            // .attr("fill", "red"),
+            update => update,
+            exit => exit.remove()
+        )
+        // .attr("x", (d) => projection(d) ? projection(d)[0] - (iconSize / 2) : null)
+        // .attr("y", (d) => projection(d) ? projection(d)[1] - (iconSize / 2) : null)
+        // .style("display", (d) => projection(d) ? "block" : "none")
+        
+
+        updateFlights();
+    }
+
+    $: if (allFlights.length > 0 && svgContainer && projection) {
+        drawFlightsOnGlobe(allFlights);
+        console.log("Flights has data");
+    }
+
+
+
+
+
+
+
+
+    async function fetchData() {
+     try {
+            const response = await fetch('/API/flights');
+
+            if (!response.ok) {
+                throw new Error('Data ophaal error');
+            }
+            if (response.ok) {
+                console.log('Data Succes')
+            }
+            const flightsData = await response.json();
+
+            allFlights = flightsData;
+            
+
+        } catch (error) {
+            console.error('Fout gegevens ophalen:', error);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     onMount( async() => {
         
     
-
-
-
+        const initialScale = Math.min(width, height) / 2.2;
 
 
         // const
@@ -77,42 +205,7 @@
 
 
 
-    const visiblethreshold = Math.PI / 2;
-
-    function updateFlights() {
-        if (!svg || !projection) return;
-
-        const center =[-projection.rotate()[0], -projection.rotate()[1]];
-        const iconSize = 30;
-
-        svg.selectAll("image.flight")
-        .each(function(d) {
-            const isVisible = d3.geoDistance(d, center) <= visiblethreshold;
-
-            if (isVisible) {
-
-                const [x, y] = projection(d);
-                d3.select(this)
-                .attr("x", x - (iconSize / 2))
-                .attr("y", y - (iconSize / 2))
-                .style("display", "block");
-            } else {
-                d3.select(this)
-                .style("display", "none")
-            }
-        })
-    }
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
@@ -259,71 +352,13 @@
         
     })
 
-    function drawFlightsOnGlobe(flights) {
-        const coordinates = flights
-        .filter(flight => flight[5] != null && flight[6] != null)
-        .map(flight => [flight[5], flight[6]]);
-
-
-        const iconSize = 30;
-
-        svg.selectAll("image.flight")
-        .data(coordinates)
-        .join(enter => enter.append("image")
-            .attr("class", "flight")
-            .attr("href", "/flight-plane-svgrepo-com.svg")
-            .attr("width", iconSize)
-            .attr("height", iconSize),
-            // .attr("r", 2)
-            // .attr("fill", "red"),
-            update => update,
-            exit => exit.remove()
-        )
-        .attr("x", (d) => projection(d) ? projection(d)[0] - (iconSize / 2) : null)
-        .attr("y", (d) => projection(d) ? projection(d)[1] - (iconSize / 2) : null)
-        .style("display", (d) => projection(d) ? "block" : "none")
-        
-
-        updateFlights();
-    }
-
-    $: if (allFlights.length > 0 && svgContainer && projection) {
-        drawFlightsOnGlobe(allFlights);
-        console.log("Flights has data");
-    }
-
-
-
-
-
-
-
-
-    async function fetchData() {
-     try {
-            const response = await fetch('/API/flights');
-
-            if (!response.ok) {
-                throw new Error('Data ophaal error');
-            }
-            if (response.ok) {
-                console.log('Data Succes')
-            }
-            const flightsData = await response.json();
-
-            allFlights = flightsData;
-            
-
-        } catch (error) {
-            console.error('Fout gegevens ophalen:', error);
-        }
-    }
-
 
 
     </script>
 
 
+
+    <svelte:window bind:innerWidth={width} bind:innerHeight={height} />
 
     <h1>Flight Tracker</h1>
 
@@ -337,18 +372,26 @@
     
 
     <style>
-        *,*::before,*::after {
+        /* *,*::before,*::after {
             box-sizing: border-box;
             transition: all 0.5s ease-in-out;
+        } */
+
+        :global(body) {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            z-index: -100;
+            width: 100vw;
+            height: 100vh;
+            background-color: darkblue;
         }
 
-        /* body {
-            background-color: darkblue;
-        } */
-
-        /* h1 {
+        h1 {
+            box-sizing: border-box;
+            transition: all 0.5s ease-in-out;
             color: white;
-        } */
+        }
         :global(#globe) {
             display: block;
             margin: 0 auto;
