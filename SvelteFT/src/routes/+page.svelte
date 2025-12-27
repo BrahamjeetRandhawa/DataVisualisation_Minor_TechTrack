e1cba2262378e366118de1dceba86bdfe3488d36
Braham <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-12-27 14:36:15 +0100

    Locate button working with animations included

--- SvelteFT/src/routes/+page.svelte
+++ SvelteFT/src/routes/+page.svelte
@@ -41,6 +41,7 @@
     let timer;
     const speedFactor = 0.0000005;
     let userLocation = null;
+    let isLocating = false;
 
     // For hover effect on aircrafts
     let hoveredFlight = null;
@@ -61,6 +62,8 @@
             return;
         }
 
+        isLocating = true;
+
         navigator.geolocation.getCurrentPosition((position) => {
             const { latitude, longitude } = position.coords;
 
@@ -69,7 +72,7 @@
             const targetRotation = [-longitude, -latitude];
 
             // Zoom in effect
-            const targetScale = initialScale * 2.5;
+            const targetScale = initialScale * 15;
 
             // Animation smoothing using d3 transition
             d3.transition()
@@ -381,7 +384,7 @@
         <p class="Description">This flighttracker gives insight about the passengers aircrafts in real time</p>
     </div> -->
 
-	<svg id="globe" bind:this={svgContainer}></svg>
+	<svg id="globe" bind:this={svgContainer} on:mousedown={() => isLocating = false} on:touchstart={() => isLocating = false} role="application" aria-label="3D Globe"></svg>
 
     <!-- With the '{#if (data)}', the code within only happens when this is triggerd on the screen. This code snippet will only show up when hovered over the aircrafts. -->
     {#if hoveredFlight && !selectedFlight}
@@ -404,6 +407,12 @@
     send={send}
     />
     {/if}
+
+    <button class="locateButton" class:active={isLocating} on:click={flyToUserLocation} aria-label="Locate me">
+        <!-- <img src="/locate-icon.svg" alt="Locate me icon" /> -->
+        
+        <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.096"></g><g id="SVGRepo_iconCarrier"> <path d="M10.9792 4.26973L4.59197 18.4636C4.10239 19.5516 3.85761 20.0955 3.9608 20.4146C4.05015 20.6908 4.2714 20.9042 4.55064 20.9836C4.87315 21.0753 5.40801 20.8112 6.47772 20.283L11.2921 17.9055C11.552 17.7771 11.682 17.713 11.8181 17.6877C11.9387 17.6653 12.0624 17.6653 12.183 17.6877C12.3192 17.713 12.4491 17.7771 12.709 17.9055L17.5234 20.283C18.5931 20.8112 19.128 21.0753 19.4505 20.9836C19.7298 20.9042 19.951 20.6908 20.0403 20.4146C20.1435 20.0955 19.8988 19.5516 19.4092 18.4636L13.0219 4.26973C12.6979 3.54967 12.5359 3.18964 12.3108 3.07837C12.1153 2.98169 11.8859 2.98169 11.6903 3.07837C11.4653 3.18964 11.3032 3.54967 10.9792 4.26973Z" stroke="white" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+    </button>
 
     
 
